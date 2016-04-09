b2e50142669c8aac72d2ac0e9e6ee7c0f86d31a7
Hector Gonzalez <hector.glez4@gmail.com>
2016-04-09 18:04:51 +0200

    controller and htaccess

--- index.php
+++ index.php
@@ -1,61 +1,48 @@
-<!DOCTYPE html>
-<html >
-    <?php
-    session_start();
-    if (isset($_SESSION['name'])) {
-        session_destroy();
-    }
-    if (isset($_POST['user'])) {
-        $db = new PDO('mysql:host=localhost;dbname=mysql', "root", "");
-        $req = $db->query("SELECT * FROM user WHERE User = '" . $_POST['user'] . "' AND Password = Password('" . $_POST['pass'] . "')");
-        $res = $req->fetch();
-        if ($res) {
-            $_SESSION['name'] = $res['User'];
-            header('Location: system/index.php');
-        } else {
-            return false;
-        }
-    }
-    ?>
-    <head>
-        <meta charset="UTF-8">
-        <title>Horse</title>
-        <link rel="stylesheet" href="css/style.css">
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <link href="assets/css/style.css" rel="stylesheet">
-        <script type="text/javascript" src="assets/js/js.js"></script>
-    </head>
-    <body style="background: #2c3e50">
-        <div class="row " style="margin-top: 100px">
-            <div class="col-md-12">
-                <div class="col-md-4 col-sm-1 col-xs-1">
-                </div>
-                <div class="col-md-6 col-sm-9 col-xs-11">
-                    <div class="formholder">
-                        <div class="randompad">
-                            <form method="post" action="">
-                                <fieldset>
-                                    <label name="email">User</label>
-                                    <input name="user" type="text" placeholder="User">
-                                    <label name="password">Password</label>
-                                    <input name="pass" type="password" placeholder="password">
-                                    <div id="validar"></div>
-                                    <input type="submit" value="Login" >
-                                </fieldset>
-                            </form>
-                        </div>
-                    </div>
+<?php
 
-                </div>
-                <div class="col-md-2">
+session_start();
 
-                </div>
-            </div>
-        </div>
-        <script src="js/index.js"></script>
-        <!--        estos js son sumamente necesarios para trabajar el bootstrap-->
-        <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-        <script src="assets/js/scripts.js"></script>
-    </body>
-</html>
+define('WEBROOT', str_replace('index.php','',$_SERVER['SCRIPT_NAME']));
+define('ROOT', str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
+require(ROOT.'php/controller/Controller.php');
+$params = explode('/', $_GET['p']);
+$controller = $params[0];
+$action = isset($params[1]) ? $params[1] : 'index';
+
+if ($controller == 'php') {
+    $controller = $params[2];
+    $action = $params[3];
+}
+
+if ($controller == '') {
+    header('Location: '.WEBROOT.'index');
+}
+
+if (!isset($_SESSION['id']) && $controller == 'index') {
+    $controller = 'User';
+}
+
+if (isset($_SESSION['id']) && $controller == 'index') {
+    $controller = 'Home';
+}
+
+/*var_dump($controller, $action);*/
+/*var_dump($_SESSION['email']);*/
+if (file_exists(ROOT.'php/controller/'.$controller.'.php')) {
+	require(ROOT.'php/controller/'.$controller.'.php');
+	$controller = new $controller();
+	if(method_exists($controller,$action)){
+	    unset($params[0]); unset($params[1]);
+	    call_user_func_array(array($controller,$action),$params);
+	}
+	else{
+	    require(ROOT.'php/view/error.php');
+	}
+}
+else
+{
+	require(ROOT.'php/view/error.php');
+}
+
+
+?>
