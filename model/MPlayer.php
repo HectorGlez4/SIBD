e5142a7a60d50a6842e21bb695486e58b77b91de
Hector Gonzalez <hector.glez4@gmail.com>
2016-04-09 12:32:54 +0200

    Model Player

--- model/MPlayer.php
+++ model/MPlayer.php
@@ -10,17 +10,52 @@ class MPlayer extends Model
 			return $this->Select($stmt);
 		}
 
-		function InsertPlayer($id_account, $title, $description )
+		function InsertPlayer($email, $password, $name, $lastname, $sex, $birthday, $phone, $mail_address,
+							$avatar, $description, $website, $ip_address, $inscription_date, $last_login_date,
+							$pseudonym, $id_infrastucture, $id_club, $id_inventary, $id_task, $id_bank_account,
+							$id_horse)
 		{
 			$this->Connect();
-			$sql = "INSERT INTO player (id_formation, title, description) 
-			VALUES (:idFormation, :title, :description);";
+			$sql = "INSERT INTO player (email, password, name, lastname, sex, birthday, phone, mail_address,
+									avatar, description, website, ip_address, inscription_date, last_login_date,
+									pseudonym, id_infrastucture, id_club, id_inventary, id_task, id_bank_account,
+									id_horse) 
+			VALUES (:email, :password, :name, :lastname, :sex, :birthday, :phone, :mail_address,
+					:avatar, :description, :website, :ip_address, :inscription_date, :last_login_date,
+					:pseudonym, :id_infrastucture, :id_club, :id_inventary, :id_task, :id_bank_account, :id_horse);";
 			$stmt = $this->PDO->prepare($sql);
-			$title = htmlspecialchars($title);
+			$email = htmlspecialchars($email);
+			$password = htmlspecialchars($password);
+			$name = htmlspecialchars($name);
+			$lastname = htmlspecialchars($lastname);
+			$mail_address = htmlspecialchars($mail_address);
+			$avatar = htmlspecialchars($avatar);
 			$description = htmlspecialchars($description);
-			$stmt->bindParam(":idFormation", $idFormation, PDO::PARAM_INT);
-			$stmt->bindParam(":title", $title, PDO::PARAM_STR);
+			$website = htmlspecialchars($website);
+			$ip_address = htmlspecialchars($ip_address);
+			$pseudonym = htmlspecialchars($pseudonym);
+			$stmt->bindParam(":email", $email, PDO::PARAM_STR);
+			$stmt->bindParam(":password", $password, PDO::PARAM_STR);
+			$stmt->bindParam(":name", $name, PDO::PARAM_STR);
+			$stmt->bindParam(":lastname", $lastname, PDO::PARAM_STR);
+			$stmt->bindParam(":sex", $sex, PDO::PARAM_BOOL);
+			$stmt->bindParam(":birthday", $birthday, PDO::PARAM_STR);
+			$stmt->bindParam(":phone", $phone, PDO::PARAM_INT);
+			$stmt->bindParam(":mail_address", $mail_address, PDO::PARAM_STR);
+			$stmt->bindParam(":avatar", $avatar, PDO::PARAM_STR);
 			$stmt->bindParam(":description", $description, PDO::PARAM_STR);
+			$stmt->bindParam(":website", $website, PDO::PARAM_STR);
+			$stmt->bindParam(":ip_address", $ip_address, PDO::PARAM_STR);
+			$stmt->bindParam(":inscription_date", $inscription_date, PDO::PARAM_STR);
+			$stmt->bindParam(":last_login_date", $last_login_date, PDO::PARAM_STR);
+			$stmt->bindParam(":pseudonym", $pseudonym, PDO::PARAM_STR);
+			$stmt->bindParam(":id_infrastucture", $id_infrastucture, PDO::PARAM_INT);
+			$stmt->bindParam(":id_club", $id_club, PDO::PARAM_INT);
+			$stmt->bindParam(":id_inventary", $id_inventary, PDO::PARAM_INT);
+			$stmt->bindParam(":id_task", $id_task, PDO::PARAM_INT);
+			$stmt->bindParam(":id_bank_account", $id_bank_account, PDO::PARAM_INT);
+			$stmt->bindParam(":id_horse", $id_horse, PDO::PARAM_INT);
+
 			return $this->Insert($stmt);
 		}
 
